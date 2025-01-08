0a4b4dc9d3e5a523504dc06b6d0376dd76149df5
Rakesh709 <kumar.rakesh1997@outlook.com>
2025-01-08 22:53:48 +0530

    mongoodb setup

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import bodyParser from "body-parser"
 import cookieParser from "cookie-parser"
 import compression from "compression"
 import cors from "cors"
+import mongoose from "mongoose";
+import { error } from "console";
 
 
 const app = express();
@@ -22,3 +24,10 @@ const server =  http.createServer(app);
 server.listen(8080,()=>{
     console.log("Server is runnning on http://localhost:8080")
 });
+
+const MONGO_URL ="mongodb+srv://galaxygalactus4:Z4GtzIOtEL8SbRzf@rest-api-cluster.iag5t.mongodb.net/?retryWrites=true&w=majority&appName=rest-api-cluster"
+
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on("error",(error:Error)=> console.log(error))
