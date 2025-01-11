bb9c91074229d0771490e26117f8e5a4a38e7d0f
Rakesh709 <kumar.rakesh1997@outlook.com>
2025-01-11 19:39:39 +0530

    login authentication

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -1,38 +1,76 @@
-import  express  from "express";
+import express from "express";
 
-import { getUserByEmail,createUser } from "../db/users";
+import { getUserByEmail, createUser } from "../db/users";
 import { authentication, random } from "../helpers";
 
-export const register  = async (req: express.Request, res: express.Response):Promise<any> =>{
+export const login = async (req: express.Request, res: express.Response):Promise<any> => {
+    try {
+
+        const {email, password} = req.body;
+
+        if(!email || !password){
+            return res.sendStatus(400)
+        }
+
+        const user = await getUserByEmail(email).select('+authentication.salt +authentication.password')
+        //select('+authentication.salt +authentication.password')
+        // above is very imp
+        if(!user){
+            return res.sendStatus(400)
+        }
+
+        //validating user without password
+        const expecetedHash = authentication(user.authentication.salt, password)
+
+        if(user.authentication.password  != expecetedHash){
+            return res.sendStatus(403)
+        }
+
+        const salt = random();
+        user.authentication.sessionToken = authentication(salt,user._id.toString());
+
+        await user.save();
+
+        res.cookie("RAKESH-AUTH",user.authentication.sessionToken,{domain:'localhost',path:'/'});
+
+        return res.status(200).json(user).end();
+
+    } catch (error) {
+        console.log(error)
+        return res.sendStatus(400);
+    }
+}
+
+export const register = async (req: express.Request, res: express.Response): Promise<any> => {
     try {
         console.log("Request Body:", req.body);
-        const {email,password,username} = req.body;
+        const { email, password, username } = req.body;
 
-        if(!email || !password || !username){
+        if (!email || !password || !username) {
             return res.sendStatus(400)
         }
 
         const existingUser = await getUserByEmail(email);
 
-        if(existingUser){
+        if (existingUser) {
             return res.sendStatus(400)
         }
 
 
-        const salt= random();
+        const salt = random();
 
         //create user 
 
         const user = await createUser({
             email,
             username,
-            authentication:{
+            authentication: {
                 salt,
-                password: authentication(salt,password),
+                password: authentication(salt, password),
             },
         });
 
-    return res.status(200).json(user).end();
+        return res.status(200).json(user).end();
 
     } catch (error) {
         console.log(error)
